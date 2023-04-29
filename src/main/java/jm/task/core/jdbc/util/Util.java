014c0c3d4675591918059d5200d00a51f219b25d
Olegechkin <olechka5.14@yandex.ru>
2023-04-29 22:24:08 +0400

    Обновила подключение

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -36,7 +36,6 @@ public class Util {
 
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
-                settings.put(Environment.HBM2DDL_AUTO, "");
 
                 configuration.setProperties(settings);
 
